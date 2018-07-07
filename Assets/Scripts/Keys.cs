b5acf256616995639282b54d6ab4ae52380ef598
JST5000 <aim4math@gmail.com>
2018-07-07 18:43:13 -0400

    Added Sharp Keys
    ^

---
+++ Assets/Scripts/Keys.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keys : MonoBehaviour {
+
+	public GameObject[] keys;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void KeyPressed(string key) {
+		switch (key) {
+		case "A":
+			break;
+		case "B":
+			break;
+		case "C":
+			break;
+		case "D":
+			break;
+		case "E":
+			break;
+		case "F":
+			break;	
+		case "G":
+			break;
+		}
+	}
+}
